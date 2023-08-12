ab9f910d57367f544ec3c6ca283c9b387665f5bf
Nitish Sharma <nit4642@gmail.com>
2023-08-13 01:25:18 +0530

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Header() {
-  return <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-400 h-32 flex justify-start items-center drop-shadow-2xl ml-2 mr-2 mt-1 rounded-md ">
+  return <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-400 h-32 flex justify-start items-center shadow-xl shadow-slate-200 mr-2 mt-1 rounded-md ">
     <h1 className="text-4xl text-white ml-8 mb-4">Keeper App</h1>
   </div>;
 }
